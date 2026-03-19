613f4d004731cda95991705b9f1cfd4cde7a2120
Jayden Knight <122502403+JaydenK842@users.noreply.github.com>
2026-03-19 12:19:35 -0400

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,5 +4,11 @@
 	let { children } = $props();
 </script>
 
+<div class="grid grid-cols-3 gap-0.5">
+    <p><a href="/dice">DICE</a></p>
+    <p><a href="/cards">CARDS</a></p>
+    <p><a href="/">8 BALL</a></p>
+</div>
+
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 {@render children()}
